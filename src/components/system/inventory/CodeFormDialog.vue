55484dbb291ccb344b065dacf4288be793b85632
jipenalver <jianchris2k14@gmail.com>
2024-11-12 21:14:03 +0800

    feat: added approval code form

---
+++ src/components/system/inventory/CodeFormDialog.vue
@@ -0,0 +1,134 @@
+<script setup>
+import { useBranchesStore } from '@/stores/branches'
+import AlertNotification from '@/components/common/AlertNotification.vue'
+import { requiredValidator, alphaDashValidator } from '@/utils/validators'
+import { formActionDefault } from '@/utils/supabase.js'
+import { ref, watch } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const props = defineProps(['isDialogVisible', 'itemData', 'tableOptions', 'tableFilters'])
+
+const emit = defineEmits(['update:isDialogVisible'])
+
+// Utilize pre-defined vue functions
+const { mdAndDown } = useDisplay()
+
+// Use Pinia Store
+const branchesStore = useBranchesStore()
+
+// Load Variables
+const formDataDefault = {
+  code: ''
+}
+const formData = ref({
+  ...formDataDefault
+})
+const formAction = ref({
+  ...formActionDefault
+})
+const refVForm = ref()
+const isUpdate = ref(false)
+
+// Monitor itemData if it has data
+watch(
+  () => props.itemData,
+  () => {
+    isUpdate.value = props.itemData ? true : false
+    formData.value = props.itemData ? { ...props.itemData } : { ...formDataDefault }
+  }
+)
+
+// Submit Functionality
+const onSubmit = async () => {
+  // Reset Form Action utils
+  formAction.value = { ...formActionDefault, formProcess: true }
+
+  //   // Check if isUpdate is true, then do update, if false do add
+  //   const { data, error } = isUpdate.value
+  //     ? await branchesStore.updateBranch(formData.value)
+  //     : await branchesStore.addBranch(formData.value)
+
+  //   if (error) {
+  //     // Add Error Message and Status Code
+  //     formAction.value.formErrorMessage = error.message
+  //     formAction.value.formStatus = error.status
+
+  //     // Turn off processing
+  //     formAction.value.formProcess = false
+  //   } else if (data) {
+  //     // Add Success Message
+  //     formAction.value.formSuccessMessage = 'Successfully Added Branch.'
+
+  //     await branchesStore.getBranchesTable(props.tableOptions, props.tableFilters)
+  //     await branchesStore.getBranches()
+
+  //     // Form Reset and Close Dialog
+  //     setTimeout(() => {
+  //       onFormReset()
+  //     }, 2500)
+  //   }
+}
+
+// Trigger Validators
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
+}
+
+// Form Reset
+const onFormReset = () => {
+  formAction.value = { ...formActionDefault }
+  emit('update:isDialogVisible', false)
+}
+</script>
+
+<template>
+  <v-dialog
+    :max-width="mdAndDown ? undefined : '600'"
+    :model-value="props.isDialogVisible"
+    :fullscreen="mdAndDown"
+    persistent
+  >
+    <v-card prepend-icon="mdi-number" title="Branch Information">
+      <AlertNotification
+        :form-success-message="formAction.formSuccessMessage"
+        :form-error-message="formAction.formErrorMessage"
+      ></AlertNotification>
+
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.code"
+                label="Name"
+                :rules="[requiredValidator, alphaDashValidator]"
+                :disabled="isUpdate"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="pa-4">
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close" variant="plain" prepend-icon="mdi-close" @click="onFormReset"></v-btn>
+
+          <v-btn
+            prepend-icon="mdi-pencil"
+            color="red-darken-4"
+            type="submit"
+            variant="elevated"
+            :disabled="formAction.formProcess"
+            :loading="formAction.formProcess"
+          >
+            {{ isUpdate ? 'Update Branch' : 'Add Branch' }}
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
