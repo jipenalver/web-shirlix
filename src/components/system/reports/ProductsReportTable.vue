280f3d8a58579ebb3791f896ba39b35986cfd0bb
jipenalver <jianchris2k14@gmail.com>
2024-12-28 00:29:45 +0800

    refactor: added products report composable

--- src/components/system/reports/ProductsReportTable.vue
+++ src/components/system/reports/ProductsReportTable.vue
@@ -1,96 +1,24 @@
 <script setup>
-import { getAvatarText, generateCSV, generateCSVTrim } from '@/utils/helpers'
-import { tableHeaders } from './productsReportTableUtils'
-import { useBranchesStore } from '@/stores/branches'
-import { useProductsStore } from '@/stores/products'
-import { useReportsStore } from '@/stores/reports'
-import { onMounted, onUnmounted, ref } from 'vue'
+import { useProductsReport } from '@/composables/system/reports/productsReport'
+import { getAvatarText } from '@/utils/helpers'
 import { useDisplay } from 'vuetify'
-import { useDate } from 'vuetify'
 
 // Utilize pre-defined vue functions
-const date = useDate()
 const { mobile } = useDisplay()
 
-// Use Pinia Store
-const productsStore = useProductsStore()
-const branchesStore = useBranchesStore()
-const reportsStore = useReportsStore()
-
-// Load Variables
-const tableOptions = ref({
-  page: 1,
-  itemsPerPage: -1,
-  sortBy: [],
-  isLoading: false
-})
-const tableFilters = ref({
-  branch_id: null,
-  product_id: null,
-  date: null
-})
-
-// Retrieve Data based on Date
-const onFilterDate = (isCleared = false) => {
-  if (isCleared) tableFilters.value.date = null
-
-  onLoadItems(tableOptions.value, tableFilters.value)
-}
-
-// Retrieve Data based on Filter
-const onFilterItems = () => {
-  onLoadItems(tableOptions.value, tableFilters.value)
-}
-
-// Load Tables Data
-const onLoadItems = async ({ page, itemsPerPage, sortBy }) => {
-  // Trigger Loading
-  tableOptions.value.isLoading = true
-
-  await reportsStore.getProductsReport({ page, itemsPerPage, sortBy }, tableFilters.value)
-
-  // Trigger Loading
-  tableOptions.value.isLoading = false
-}
-
-// CSV Data
-const csvData = () => {
-  // Get the headers from utils
-  const headers = tableHeaders.map((header) => header.title)
-  const addHeaders = ['Metric']
-  const newHeaders = [...headers, ...addHeaders].join(',')
-
-  // Get the reports data and map it to be used as csv data, follow the headers arrangement
-  const rows = reportsStore.productsReport.map((item) => {
-    // eslint-disable-next-line no-unused-vars
-    const { date: createdDate, image_url, description, ...data } = item
-
-    const arrayData = Object.values(data)
-
-    return [generateCSVTrim(date.format(createdDate, 'fullDate')), ...arrayData].join(',')
-  })
-
-  // Combine headers and csv data
-  return [newHeaders, ...rows].join('\n')
-}
-
-// Generate CSV
-const onGenerate = () => {
-  const filename = new Date().toISOString() + '-products-inventory-report'
-
-  generateCSV(filename, csvData())
-}
-
-// If Component is Unloaded
-onUnmounted(() => {
-  reportsStore.$reset()
-})
-
-// Load Functions during component rendering
-onMounted(async () => {
-  if (branchesStore.branches.length == 0) await branchesStore.getBranches()
-  if (productsStore.products.length == 0) await productsStore.getProducts()
-})
+// Utilized Composable
+const {
+  date,
+  tableHeaders,
+  tableOptions,
+  tableFilters,
+  onFilterDate,
+  onFilterItems,
+  onGenerate,
+  productsStore,
+  branchesStore,
+  reportsStore
+} = useProductsReport()
 </script>
 
 <template>
@@ -106,7 +34,6 @@ onMounted(async () => {
         :items="reportsStore.productsReport"
         :items-length="reportsStore.productsReport.length"
         no-data-text="Use the above filter to display report"
-        @update:sort-by="(sortBy) => onLoadItems({ sortBy })"
         hide-default-footer
         :hide-default-header="mobile"
         :mobile="mobile"
