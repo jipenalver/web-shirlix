42d53f9287a995ba260e3b8df0beb7396cbe9380
jipenalver <jianchris2k14@gmail.com>
2024-12-27 19:55:55 +0800

    feat: added stock transfer and products report files

--- src/views/system/reports/ProductsReportView.vue
+++ src/views/system/reports/ProductsReportView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ProductsReportTable from '@/components/system/reports/ProductsReportTable.vue'
 import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
 import AppLayout from '@/components/layout/AppLayout.vue'
 import { useDisplay } from 'vuetify'
@@ -39,7 +40,9 @@ const isDrawerVisible = ref(mobile.value ? false : true)
         </v-card>
 
         <v-card>
-          <v-card-text> </v-card-text>
+          <v-card-text>
+            <ProductsReportTable></ProductsReportTable>
+          </v-card-text>
         </v-card>
       </v-container>
     </template>
